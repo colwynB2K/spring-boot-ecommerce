1b84ba561c71b06d43e01405ac874253d4d4c6fa
eersels3764 <ivo.eersels@idewe.be>
2021-09-07 16:17:29 +0200

    Add JPA Entities
    - Replace @Data in ProductCategory with
    @Getter
    @Setter
    
    known bug -> If you use @Data you will get an infinite loop when fetching the products from a category due to the bidirectional nature of the relationship

--- src/main/java/com/luv2code/ecommerce/domain/ProductCategory.java
+++ src/main/java/com/luv2code/ecommerce/domain/ProductCategory.java
@@ -1,13 +1,17 @@
 package com.luv2code.ecommerce.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "product_category")
-@Data
+// @Data -- known bug -> If you use Data you will get an infinite loop when fetching the products from a category due to the bidirectional nature of the relationship
+@Getter
+@Setter
 public class ProductCategory {
 
     @Id
@@ -18,6 +22,6 @@ public class ProductCategory {
     @Column(name = "category_name")
     private String name;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productCategory")
     private Set<Product> products;
 }
