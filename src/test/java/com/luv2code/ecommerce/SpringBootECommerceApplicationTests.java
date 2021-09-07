f95c6e6135b3dd823624cf4c4224c93148978c2d
eersels3764 <ivo.eersels@idewe.be>
2021-09-07 15:27:18 +0200

    Initial commit including - Spring Initializr skeleton - db scripts - static web assets

---
+++ src/test/java/com/luv2code/ecommerce/SpringBootECommerceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.luv2code.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
