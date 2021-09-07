088e63110334afb919f41d24e4c7bad4b2572cb7
eersels3764 <ivo.eersels@idewe.be>
2021-09-07 16:50:10 +0200

    REST APIs - Configure for Read Only - Overview
    	So no Create, Update, Delete support for now! => Disable POST, PUT and DELETE
    
    	1. Don't use Spring Data Rest
    		- Manually create @RestController
    		- Manually define request mappings
    		- YOU WILL LOSE Spring Data REST support for paging, sorting, etc...!!!
    
    	2. Disable HTTP methods in Spring Data REST
    
    		package com.luv2code.ecommerce.config;
    
    		import com.luv2code.ecommerce.domain.Product;
    		import org.springframework.context.annotation.Configuration;
    		import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
    		import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
    		import org.springframework.http.HttpMethod;
    		import org.springframework.web.servlet.config.annotation.CorsRegistry;
    
    		@Configuration
    		public class DataRestConfig implements RepositoryRestConfigurer {
    
    			@Override
    			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
    				HttpMethod[] theUnsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
    
    				// Disable HTTP methods for Product: POST, PUT and DELETE
    				config.getExposureConfiguration()
    						.forDomainType(Product.class)
    						.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
    						.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
    			}
    		}

---
+++ src/main/java/com/luv2code/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,23 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.domain.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        // Disable HTTP methods for Product: POST, PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+    }
+}
