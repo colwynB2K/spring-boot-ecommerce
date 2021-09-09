b9ce80b5eab05450db03f6b1e614bc4d67973df3
eersels3764 <ivo.eersels@idewe.be>
2021-09-09 11:53:00 +0200

    Add CrossOrigin (CORS) support to Spring Boot App
    - By default, the current code will fail!
    			WEB BROWSERS WILL NOT ALLOW SCRIPT CODE TO CALL APIs  NOT ON THE SAME ORIGIN OR DOMAIN = Same-Origin SECURITY policy  (This is specific to JavaScript running in a web browser)
    			origin = scheme/protocol, hostname, port => http://localhost:4200 of Angular Server which runs locally is NOT the same origin as http://localhost:8080 of Tomcat Server which normally runs remotely!!!
    
    			We can relax this browser behaviour by adding the 'Cross-Origin Resource Sharing (CORS)' on the server side application.
    			Which adds a HTTP header to the server responses which basically whitelists specified origins to access your server origin. So this will allow requests from WEB BROWSER SCRIPTS from this origin.
    
    			You just need to add @CrossOrigin annotation with whitelisted client origins as a value
    
    			@CrossOrigin("http://localhost:4200")															// Allow requests from WEB BROWSER SCRIPTS from this origin.
    			public interface ProductRepository extends JpaRepository<Product, Long> {
    			}
    
    			Allow multiple origins via comma separated list
    			@CrossOrigin({"http://localhost:4200", http://www.whatever.com})
    
    			Allow ALL ORIGINS! Don't specify a origin, this is the default which allows access from any website
    			@CrossOrigin

--- src/main/java/com/luv2code/ecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/com/luv2code/ecommerce/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.luv2code.ecommerce.repository;
 import com.luv2code.ecommerce.domain.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(path = "product-categories", collectionResourceRel = "categories")
 // Expose Repository as REST API endpoint:
 // specify endpoint path (otherwise it will like just stick an 's' to the end like '/productCategories'.
--- src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.luv2code.ecommerce.repository;
 
 import com.luv2code.ecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
