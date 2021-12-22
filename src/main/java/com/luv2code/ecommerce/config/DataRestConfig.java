6d13da42dbcf263dcd7e7197a78d9f513d3bf99a
eersels3764 <ivo.eersels@idewe.be>
2021-12-22 16:54:19 +0100

    eCommerce Project - Search for Products by Category (Dynamic Search Component) ______________________________________________________________________________
    Angular Project - Search for Products by Category (Dynamic Search Component) -> Get all cats from DB and print cat links for them
    ----------------------------------------------------------------------------
    
    	* Development Process
    
    		1) Modify Spring Boot app - Expose entity ids
    
    			By default, Spring Data REST doesn't expose entity ids (however these can be usefull to get products or categories by id)
    
    			http://localhost:8080/api/product-categories returns
    			{
    			  "_embedded" : {
    				"categories" : [ {
    				  "name" : "Books",
    				  "_links" : {
    					"self" : {
    					  "href" : "http://localhost:8080/api/product-categories/1"
    					},
    					"productCategory" : {
    					  "href" : "http://localhost:8080/api/product-categories/1"
    					},
    					"products" : {
    					  "href" : "http://localhost:8080/api/product-categories/1/products"
    					}
    				  }
    				}, {
    				  "name" : "Coffee Mugs",
    				  "_links" : {
    					"self" : {
    					  "href" : "http://localhost:8080/api/product-categories/2"
    					},
    					"productCategory" : {
    					  "href" : "http://localhost:8080/api/product-categories/2"
    					},
    					"products" : {
    					  "href" : "http://localhost:8080/api/product-categories/2/products"
    					}
    				  }
    				}...
    
    			So you can only see the ids in the '_links' sections (HATEOAS).
    
    			We need to update the Spring Data REST config to expose entity ids using the JPA EntityManager
    
    				@Configuration
    				public class DataRestConfig implements RepositoryRestConfigurer {
    
    					private EntityManager entityManager;
    
    					@Autowired
    					public DataRestConfig(EntityManager entityManager) {		// Constructor Injection
    						this.entityManager = entityManager;
    					}
    
    					@Override
    					public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
    						HttpMethod[] theUnsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
    
    						// Disable HTTP methods for Product: POST, PUT and DELETE
    						...
    
    						// Disable HTTP methods for ProductCategory: POST, PUT and DELETE
    						...
    
    						// Expose entity ids
    						config.exposeIdsFor(getAllEntityClasses().toArray(new Class[0]));
    					}
    
    					private Set<Class> getAllEntityClasses() {
    						Set<Class> entityClasses = new HashSet<>();
    
    						// Get a list of all entity classes from the EntityManager
    						Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities;
    
    						// For each entity class, add it's java type (FQDN?) to the list
    						for (EntityType currentEntity : entities) {
    							entityClasses.add(currentEntity.getJavaType());
    						}
    
    						return entityClasses;
    					}
    				}

--- src/main/java/com/luv2code/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,27 @@ package com.luv2code.ecommerce.config;
 
 import com.luv2code.ecommerce.domain.Product;
 import com.luv2code.ecommerce.domain.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {		// Constructor Injection
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +39,23 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+
+        // Expose entity ids
+        config.exposeIdsFor(getAllEntityClasses().toArray(new Class[0]));
+    }
+
+    private Set<Class> getAllEntityClasses() {
+        Set<Class> entityClasses = new HashSet<>();
+
+        // Get a list of all entity classes from the EntityManager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // For each entity class, add it's java type (FQDN?) to the list
+        for (EntityType currentEntity : entities) {
+            entityClasses.add(currentEntity.getJavaType());
+        }
+
+        return entityClasses;
     }
 }
+
